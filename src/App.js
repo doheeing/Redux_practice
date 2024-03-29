0e7f966b793c847ef8034d8d9a0eac7ff6f779f8
doheeing <doheeehan@naver.com>
2024-03-29 23:59:28 +0900

    CSS 추가

--- src/App.js
+++ src/App.js
@@ -2,26 +2,42 @@ import "./App.css";
 import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import Box from "./component/Box";
+import { Container } from "react-dom";
 
 function App() {
   let count = useSelector((state) => state.count);
   let id = useSelector((state) => state.id);
-  let password = useSelector((state) => state.password);
+  // let password = useSelector((state) => state.password);
   const dispatch = useDispatch();
-  const login = () => {
-    dispatch({ type: "LOGIN", payload: { id: "dohee", password: "123" } });
-  };
+  // const login = () => {
+  //   dispatch({ type: "LOGIN", payload: { id: "dohee", password: "123" } });
+  // };
 
   const counter = () => {
     dispatch({ type: "INCREMENT", payload: { num: 5 } });
   };
+  const decounter = () => {
+    dispatch({ type: "DECREMENT", payload: { num: 1 } });
+  };
+  const reset = () => {
+    dispatch({ type: "RESET" });
+  };
   return (
-    <div>
-      <div>{id}, {password}</div>
-      <button onClick={counter}>click</button>
-      <button onClick={login}>Login</button>
-      <div>{count}</div>
-      <Box />
+    <div className="container">
+      <div>
+        <h1>{count}</h1>
+      </div>
+      <div>
+        <button onClick={counter}>increase</button>
+        <button onClick={decounter}>decrease</button>
+        <button onClick={reset}>reset</button>
+        {/* <button onClick={login}>Login</button> */}
+      </div>
+
+      {/* <div>
+        {id}, {password}
+      </div> */}
+      {/* <Box /> */}
     </div>
   );
 }
--- src/redux/reducer/reducer.js
+++ src/redux/reducer/reducer.js
@@ -2,17 +2,22 @@ import { act } from "react-dom/test-utils";
 
 let initialState = {
   count: 0,
-  id :"", 
-  password:""
+  // id :"", 
+  // password:""
 };
 
 function reducer(state = initialState, action) {
   console.log("action", action);
   if (action.type == "INCREMENT") {
     return { ...state, count: state.count + action.payload.num};
-  }else if (action.type == "LOGIN"){
-    return{...state, id:action.payload.id, password:action.payload.password}
+  }else if(action.type == "DECREMENT"){
+    return { ...state, count: state.count - action.payload.num};
+  }else if(action.type == "RESET"){
+    return { ...state, count: 0};
   }
+  // }else if (action.type == "LOGIN"){
+  //   return{...state, id:action.payload.id, password:action.payload.password}
+  //}
   return { ...state };
   // state의 나머지 값은 유지 하되, state에만 1 더하기
 }
