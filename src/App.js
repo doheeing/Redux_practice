226ad593c1cac710bb8d4f3e57ce64aca1d99f1d
doheeing <doheeehan@naver.com>
2024-03-31 16:20:06 +0900

    input 값 받아 increase, decrease 기능 구현

--- src/App.js
+++ src/App.js
@@ -5,6 +5,10 @@ import Box from "./component/Box";
 import { Container } from "react-dom";
 
 function App() {
+  const [number, setNumber] = useState(0);
+  const [insertIncreaseNum, setInsertIncreaseNum] = useState(0);
+  const [insertDecreaseNum, setInsertDecreaseNum] = useState(0);
+  const [plusMinus, setPlusMinus] = useState("");
   let count = useSelector((state) => state.count);
   let id = useSelector((state) => state.id);
   // let password = useSelector((state) => state.password);
@@ -12,14 +16,26 @@ function App() {
   // const login = () => {
   //   dispatch({ type: "LOGIN", payload: { id: "dohee", password: "123" } });
   // };
-
-  const counter = () => {
-    dispatch({ type: "INCREMENT", payload: { num: 5 } });
+  const saveInputValue = (e) => {
+    setNumber(e.target.value);
   };
-  const decounter = () => {
-    dispatch({ type: "DECREMENT", payload: { num: 1 } });
+  const counter = () => {
+    setInsertIncreaseNum(number);
+    dispatch({
+      type: "INCREMENT",
+      payload: { num: Number(insertIncreaseNum) },
+    });
+  };
+  const decounter = (insertNum) => {
+    setInsertDecreaseNum(number);
+    dispatch({
+      type: "DECREMENT",
+      payload: { num: Number(insertDecreaseNum) },
+    });
   };
   const reset = () => {
+    setNumber("");
+    setPlusMinus("");
     dispatch({ type: "RESET" });
   };
   return (
@@ -28,12 +44,23 @@ function App() {
         <h1>{count}</h1>
       </div>
       <div>
-        <button onClick={counter}>increase</button>
-        <button onClick={decounter}>decrease</button>
+        <input
+          type="number"
+          onChange={saveInputValue}
+          placeholder="더하거나 뺼 숫자를 입력하세요"
+          className="input-num"
+        />
+        {/* <div>위 숫자를 더하기 or 뺴기</div>
+        <button onClick={increaseNum}>+</button>
+        <button onClick={decreaseNum}>-</button> */}
+      </div>
+      <div>
+
+        <button onClick={counter}>더하기</button>
+        <button onClick={decounter}>빼기</button>
         <button onClick={reset}>reset</button>
         {/* <button onClick={login}>Login</button> */}
       </div>
-
       {/* <div>
         {id}, {password}
       </div> */}
